3b147a4fae16a9e2a1a0de33dbcd105164d995fe
Nikola Mitić <101733519+miticn@users.noreply.github.com>
2024-06-15 17:01:03 +0200

    Refactoring

--- simulator/main.py
+++ simulator/main.py
@@ -7,6 +7,8 @@ import time
 
 import csv
 from collections import defaultdict
+from concurrent.futures import ProcessPoolExecutor
+
 
 
 def simulate(K:int, alpha: int, r: int, P:np.ndarray, time_min : int = 30, MAX_K: int = 5):
@@ -49,57 +51,93 @@ def write_results_to_csv(filename, header, results):
             writer.writerow(result)
 
 
-header = ['K', 'r', 'T', 'Ucpu', 'Uds1', 'Uds2', 'Uds3', 'Udu1', 'Udu2', 'Udu3', 'Udu4', 'Udu5',
-          'Xcpu', 'Xds1', 'Xds2', 'Xds3', 'Xdu1', 'Xdu2', 'Xdu3', 'Xdu4', 'Xdu5',
-          'Jcpu', 'Jds1', 'Jds2', 'Jds3', 'Jdu1', 'Jdu2', 'Jdu3', 'Jdu4', 'Jdu5']
+def simulate_single_run(header, r_list, alpha_k, P, time_min = 30):
 
-start_time = time.time()
+    start_time = time.time()
+    results = []
 
-#create dictionary of P matrixes
-P = {}
-for i in range(2, 6):
-    P[i] = get_P_matrix(i)
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(simulate, k, alpha_k[k], r, P[k], time_min) for k in range(2, 6) for r in r_list]
+        for future in futures:
+            results.append(future.result())
 
-alpha_k = {2:0.07919999999999999, 3:0.1188, 4:0.15839999999999999, 5:0.1584}
-r_list = [0.30, 0.55, 0.80, 1.00]
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time} seconds")
 
-results = []
+    file_name = "rezultati_simulacija.csv"
+    write_results_to_csv(file_name, header, results)
 
-from concurrent.futures import ProcessPoolExecutor
-with ProcessPoolExecutor() as executor:
-    futures = [executor.submit(simulate, k, alpha_k[k], r, P[k]) for k in range(2, 6) for r in r_list]
-    for future in futures:
-        results.append(future.result())
-
-end_time = time.time()
-execution_time = end_time - start_time
-write_results_to_csv('rezultati_simulacija.csv', header, results)
-print(f"Execution time: {execution_time} seconds")
+    print("Results written to file:", file_name)
 
 
-#100 iterations
-start_time = time.time()
-iterations = 100
-results = defaultdict(list)
 
-with ProcessPoolExecutor() as executor:
-    futures = [executor.submit(simulate, k, alpha_k[k], r, P[k]) for k in range(2, 6) for r in r_list for i in range(iterations)]
-    for future in futures:
-        result = future.result()
-        key = (result[0], result[1])  # Use (k, r) as the key
-        results[key].append(result[2:])  # Store the rest of the result
+def simulate_100_iterations(header, r_list, alpha_k, P, time_min = 30):
+    #100 iterations
+    start_time = time.time()
+    iterations = 100
+    results = defaultdict(list)
 
-averaged_results = []
-for key, value_list in results.items():
-    value_array = np.array(value_list)
-    averaged_values = np.mean(value_array, axis=0)
-    averaged_results.append([key[0], key[1]] + averaged_values.tolist())
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(simulate, k, alpha_k[k], r, P[k], time_min) for k in range(2, 6) for r in r_list for i in range(iterations)]
+        for future in futures:
+            result = future.result()
+            key = (result[0], result[1])  # Use (k, r) as the key
+            results[key].append(result[2:])  # Store the rest of the result
 
-
-end_time = time.time()
-execution_time = end_time - start_time
+    averaged_results = []
+    for key, value_list in results.items():
+        value_array = np.array(value_list)
+        averaged_values = np.mean(value_array, axis=0)
+        averaged_results.append([key[0], key[1]] + averaged_values.tolist())
 
 
-print(f"Execution time: {execution_time} seconds")
+    end_time = time.time()
+    execution_time = end_time - start_time
 
-write_results_to_csv('rezultati_simulacija_usrednjeno.csv', header, averaged_results)
+
+    print(f"Execution time: {execution_time} seconds")
+
+    file_name = "rezultati_simulacija_usrednjeno.csv"
+    write_results_to_csv(file_name, header, averaged_results)
+
+    print("Results written to file:", file_name)
+
+
+
+if __name__ == '__main__':
+
+    header = ['K', 'r', 'T', 'Ucpu', 'Uds1', 'Uds2', 'Uds3', 'Udu1', 'Udu2', 'Udu3', 'Udu4', 'Udu5',
+            'Xcpu', 'Xds1', 'Xds2', 'Xds3', 'Xdu1', 'Xdu2', 'Xdu3', 'Xdu4', 'Xdu5',
+            'Jcpu', 'Jds1', 'Jds2', 'Jds3', 'Jdu1', 'Jdu2', 'Jdu3', 'Jdu4', 'Jdu5']
+
+    #create dictionary of P matrixes
+    P = {}
+    for i in range(2, 6):
+        P[i] = get_P_matrix(i)
+
+    alpha_k = {2:0.07919999999999999, 3:0.1188, 4:0.15839999999999999, 5:0.1584}
+    r_list = [0.30, 0.55, 0.80, 1.00]
+
+    time_min = 30
+
+    # Interactive menu
+    while True:
+        print("Simulation Menu")
+        print("1. Set simulated time. (current value: ", time_min, " minutes)")
+        print("2. Simulate a single run")
+        print("3. Simulate 100 iterations")
+        print("4. Exit")
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == '1':
+            time_min = int(input("Enter minimum time for simulation: "))
+        elif choice == '2':
+            simulate_single_run(header, r_list, alpha_k, P, time_min)
+        elif choice == '3':
+            simulate_100_iterations(header, r_list, alpha_k, P, time_min)
+        elif choice == '4':
+            print("Exiting the menu.")
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
